5c09e6fc00e5c36fee4219e81f2f28bacf68181b
LucasAdamyMartin <lucasadamy123@gmail.com>
2025-03-09 16:57:15 +0100

    more responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,52 +19,54 @@ export function Header() {
 
     return(
         <header>
-        <div id="leftHeader">
-          <h1 id="title">sneakers</h1>
-          <div id="navigation">
-            {navigation_items.map((item) => (
-              <div key={item} id={item}>
-                <a href="/">{item}</a>
+          <div id="containerHeader">
+            <div id="leftHeader">
+              <h1 id="title">sneakers</h1>
+              <div id="navigation">
+                {navigation_items.map((item) => (
+                  <div key={item} id={item}>
+                    <a href="/">{item}</a>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
-        <div id="rightHeader">
-          <button onClick={handleChange} id="cartButton">
-            <img src="/images/icon-cart.svg" alt="cart"/>
-            {showProduct && <div>{countPanier}</div>}
-          </button>
-          <a href="/">
-            <img id="profile" src="/images/image-avatar.png" alt="profile"/>
-          </a>
-        </div>
-        {cartIsOpen && 
-          <div id="cartBox">
-            <header id="headerCart">
-              <p id="titleCart">
-                Cart
-              </p>
-            </header>
-            <div id="bodyCart">
-              {showProduct ? 
-              <>
-              <div id="firstPart">
-                <img src='images/image-product-1-thumbnail.jpg' alt="product"/>
-                <div>
-                  <h1>Fall Limited Edition Sneakers</h1>
-                  <p>$125.00 x {countPanier} <strong>${125 * countPanier}.00</strong></p>
-                </div>
-                <button onClick={() => (setShowProduct(false))}>
-                  <img src='images/icon-delete.svg' alt="delete"/>
-                </button> 
-              </div>
-              <div id="secondPart">
-                <button>Checkout</button>
-              </div> 
-              </> : <p>Your cart is empty.</p>}
             </div>
-          </div>
-          }
+            <div id="rightHeader">
+              <button onClick={handleChange} id="cartButton">
+                <img src="/images/icon-cart.svg" alt="cart"/>
+                {showProduct && <div>{countPanier}</div>}
+              </button>
+              <a href="/">
+                <img id="profile" src="/images/image-avatar.png" alt="profile"/>
+              </a>
+            </div>
+            {cartIsOpen && 
+              <div id="cartBox">
+                <header id="headerCart">
+                  <p id="titleCart">
+                    Cart
+                  </p>
+                </header>
+                <div id="bodyCart">
+                  {showProduct ? 
+                  <>
+                  <div id="firstPart">
+                    <img src='images/image-product-1-thumbnail.jpg' alt="product"/>
+                    <div>
+                      <h1>Fall Limited Edition Sneakers</h1>
+                      <p>$125.00 x {countPanier} <strong>${125 * countPanier}.00</strong></p>
+                    </div>
+                    <button onClick={() => (setShowProduct(false))}>
+                      <img src='images/icon-delete.svg' alt="delete"/>
+                    </button> 
+                  </div>
+                  <div id="secondPart">
+                    <button>Checkout</button>
+                  </div> 
+                  </> : <p>Your cart is empty.</p>}
+                </div>
+              </div>
+              }
+        </div>
       </header>
     )
 }
